d5c422d1bcbd553c228f37f15f8d8e454fff5106
anilcanipsalali <anilcan.ipsalali@gmail.com>
2023-01-06 14:44:20 +0300

    Optimize imports.

--- src/main/java/com/uygunaldim/entity/Role.java
+++ src/main/java/com/uygunaldim/entity/Role.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "ROLE")
