f0a475e6350aa6baa5b301c524560706ea88c172
anilcanipsalali <anilcan.ipsalali@gmail.com>
2023-01-08 15:28:39 +0300

    Vendor added to product.

--- src/main/java/com/uygunaldim/dto/request/MarketRequest.java
+++ src/main/java/com/uygunaldim/dto/request/MarketRequest.java
@@ -1,11 +1,9 @@
 package com.uygunaldim.dto.request;
 
-import com.uygunaldim.dto.MarketProductDto;
 import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
-import java.util.List;
 
 @Data
 @Builder
@@ -13,5 +11,4 @@ public class MarketRequest {
     private Long id;
     @NotEmpty(message = "Market name cannot be empty!")
     private String name;
-    private List<MarketProductDto> products;
 }
