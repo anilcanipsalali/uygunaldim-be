523995ed9f2a02c1ed793deea2aba976229de2ed
anilcanipsalali <anilcan.ipsalali@gmail.com>
2023-03-20 15:14:36 +0300

    Util package created. ApplicationConstants utility class added.

--- src/main/java/com/uygunaldim/exception/GlobalControllerAdvice.java
+++ src/main/java/com/uygunaldim/exception/GlobalControllerAdvice.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static com.uygunaldim.exception.ExceptionResponseHeaderEnum.RETURN_CODE;
-import static com.uygunaldim.exception.ExceptionResponseHeaderEnum.RETURN_MESSAGE;
+import static com.uygunaldim.util.ApplicationConstants.RETURN_CODE;
+import static com.uygunaldim.util.ApplicationConstants.RETURN_MESSAGE;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalControllerAdvice {
@@ -18,8 +19,8 @@ public class GlobalControllerAdvice {
     public ResponseEntity<String> handleGlobalException(Exception e) {
         log.error("Exception: {}", e.getMessage());
         HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.add(RETURN_CODE.getHeader(), "UYGNALDM-00500");
-        responseHeaders.add(RETURN_MESSAGE.getHeader(), e.getMessage());
+        responseHeaders.add(RETURN_CODE, "UYGNALDM-00500");
+        responseHeaders.add(RETURN_MESSAGE, e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(responseHeaders).build();
     }
 
@@ -27,8 +28,8 @@ public class GlobalControllerAdvice {
     public ResponseEntity<String> methodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.error("MethodArgumentNotValidException: {}", e.getMessage());
         HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.add(RETURN_CODE.getHeader(), "UYGNALDM-00400");
-        responseHeaders.add(RETURN_MESSAGE.getHeader(), e.getMessage());
+        responseHeaders.add(RETURN_CODE, "UYGNALDM-00400");
+        responseHeaders.add(RETURN_MESSAGE, e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(responseHeaders).build();
     }
 
@@ -36,8 +37,8 @@ public class GlobalControllerAdvice {
     public ResponseEntity<String> handleParameterNotFoundException(UygunAldimException e) {
         log.error("UygunAldimException: {}", e.getMessage());
         HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.add(RETURN_CODE.getHeader(), e.getErrorCode());
-        responseHeaders.add(RETURN_MESSAGE.getHeader(), e.getErrorMessage());
+        responseHeaders.add(RETURN_CODE, e.getErrorCode());
+        responseHeaders.add(RETURN_MESSAGE, e.getErrorMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(responseHeaders).build();
     }
 
@@ -45,8 +46,8 @@ public class GlobalControllerAdvice {
     public ResponseEntity<String> handleAlreadyExistsException(UygunAldimException e) {
         log.error("UygunAldimException: {}", e.getMessage());
         HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.add(RETURN_CODE.getHeader(), e.getErrorCode());
-        responseHeaders.add(RETURN_MESSAGE.getHeader(), e.getErrorMessage());
+        responseHeaders.add(RETURN_CODE, e.getErrorCode());
+        responseHeaders.add(RETURN_MESSAGE, e.getErrorMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(responseHeaders).build();
     }
 }
---
+++ src/main/java/com/uygunaldim/util/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.uygunaldim.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ApplicationConstants {
+    public final static String RETURN_CODE = "uygunaldim-return-code";
+    public final static String RETURN_MESSAGE = "uygunaldim-return-message";
+}
